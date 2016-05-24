558828a7feccd0a5523a85bd505259186ba943e7
Scott Cho <scottcho@172-16-87-126.DYNAPOOL.NYU.EDU>
2016-05-24 02:14:33 -0400

    more styling

--- client/js/ranking.js
+++ client/js/ranking.js
@@ -1,7 +1,36 @@
- // Template.ranking.events({
+import { Template } from 'meteor/templating';
+ 
+import '../html/ranking.html';
 
-	//  $('.rankImage').on('click', function(){
-	//  	console.log("something");
-	//  });
+Template.photoClick.helpers({
+	
+  comments: [
+    { text: 'This is comment 1 This is comment 1 This is comment 1 This is comment 1 This is comment 1 ', name: 'Scott', time: '10/20/2015'},
+    { text: 'This is comment 2 This is comment 2 This is comment 2 This is comment 2', name: 'Gilfoyle', time: '10/20/2015'},
+    { text: 'This is comment 3', name: 'Jay', time: '10/20/2015'},
+    { text: 'This is comment 4 This is comment 4 ', name: 'Dinesh', time: '10/20/2015'},
+    { text: 'This is comment 5 This is comment 5 This is comment 5 This is comment 5 This is comment 5 This is comment 5 This is comment 5 ', name: 'McLovin', time: '10/20/2015'},
+    { text: 'This is comment 6', name: 'Richard', time: '10/20/2015'}
+  ]
 
- // });
+});
+
+Template.ranking.helpers({
+
+  images: [
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' },
+  	{ image: 'http://c1.staticflickr.com/9/8450/8026519634_f33f3724ea_b.jpg' }
+
+  ]
+
+});
