551d4bc83eb2b6efb585aa437fd1799ff3a46fa1
Scott Cho <scottsgcho@gmail.com>
2016-10-14 03:58:13 -0400

    changing post

--- client/js/post.js
+++ client/js/post.js
@@ -2,13 +2,13 @@ import '../html/post.html';
 
 if (Meteor.isClient) {
 
-	Template.post.helpers({
+	Template.postPhoto.helpers({
 
 		//Scroll helper for how many people to see the post
 		howManyNumberValue: function(){
 
 			if (Session.get('howManyNumberValue') == null) {
-				Session.set('howManyNumberValue', 50);
+				Session.set('howManyNumberValue', 100);
 				return Session.get('howManyNumberValue');
 			} else {
 				return Session.get('howManyNumberValue');
@@ -17,8 +17,8 @@ if (Meteor.isClient) {
 	});
 
 	//Scroll for dynamic input range
-	Template.post.events({
-		'input .postRange': function(event){
+	Template.postPhoto.events({
+		'input #postPhotoRatingRange': function(event){
 	    	var howManyValue = event.currentTarget.value;
 	    	console.log(howManyValue);
 	     	Session.set('howManyNumberValue', howManyValue);
